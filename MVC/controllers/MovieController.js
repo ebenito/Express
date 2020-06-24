8e960d5e761c9e3c531eeacb0ede708f87ae1cc7
Esteban Benito <ebenito@outlook.com>
2020-06-24 20:13:30 +0200

    Añado logger con MORGAN

--- MVC/controllers/MovieController.js
+++ MVC/controllers/MovieController.js
@@ -1,35 +1,49 @@
 const MovieModel = require('../models/MovieModel');
 const MovieController = {};
 
-MovieController.getAll = (req, res) => {    
+MovieController.getAll = (req, res, next) => { 
     res.json(MovieModel.findAll());
+    next(getFecha() + ' Movie - getAll');
 }
 
-MovieController.getPage = (req, res) => {
+MovieController.getPage = (req, res, next) => {
     const { nPage } = req.params;      
     res.json(MovieModel.getPage(nPage)); 
+    next(getFecha() + ' Movie - getPage: ' + nPage);
 }
 
-MovieController.getById = (req, res) => {
+MovieController.getById = (req, res, next) => {
     const { id } = req.params;  //Alternativa: const id = req.params.id;  
     res.json(MovieModel.findBy(id)); 
+    next(getFecha() + ' Movie - getById: ' + id);
 }
 
-MovieController.create = (req, res) => {
+MovieController.create = (req, res, next) => {
     const { body } = req;   //en la petición post enviar en body un json, ej: {"id":"101","title":"Cantando bajo la lluvia","year":1955,"description":"Actores inventan el cine músical por necesidad.","duration":130,"contentRate":12,"genre":"Drama"}
     res.json(MovieModel.insert({...body}));
+    next(getFecha() + ' Movie - create: ' + body.title);
 }
 
-MovieController.update = (req, res) => {    
+MovieController.update = (req, res, next) => {    
     const id = parseInt(req.params.id); 
     const { body } = req;  //en la petición put enviar en body un json, ej: {"title":"Cantando bajo la lluvia", "year": 1952}; el ID va en la ruta URL
     
     res.json(MovieModel.update({id, ...body}));
+    next(getFecha() + ' Movie - update - ID: ' + id);
 }
 
-MovieController.delete = (req, res) => {
+MovieController.delete = (req, res, next) => {
     const { id } = req.params;      
     res.json(MovieModel.delete(id));
+    next(getFecha() + ' Movie - delete - ID: ' + id);
+}
+
+function getFecha()
+{
+    const fecha = new Date();
+    // const options = { year: 'numeric', month: 'long', day: 'numeric'};
+    // return fecha.toLocaleDateString("es-ES", options);
+    return fecha;
 }
 
 module.exports = MovieController;
--- MVC/views/MovieRouter.js
+++ MVC/views/MovieRouter.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const logger = require('morgan');
 const MovieController = require('../controllers/MovieController');
 
+
 //Devuelve toda la BD de peliculas
-router.get('/', MovieController.getAll);
+router.get('/', MovieController.getAll, logger('log'));
 
 //RETO: Consulta listado por paginacion
-router.get('/page/:nPage', MovieController.getPage);
+router.get('/page/:nPage', MovieController.getPage, logger('log'));
 
 //Consulta item por ID
-router.get('/:id', MovieController.getById);
+router.get('/:id', MovieController.getById, logger('log'));
 
 //Añadir items
-router.post('/', MovieController.create);
+router.post('/', MovieController.create, logger('log'));
 
 //Modificar item
-router.put('/:id', MovieController.update);
+router.put('/:id', MovieController.update, logger('log'));
 
 //Borrar un item
-router.delete('/:id', MovieController.delete);
+router.delete('/:id', MovieController.delete, logger('log'));
 
 module.exports = router;
